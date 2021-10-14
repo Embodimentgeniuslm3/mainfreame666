055b2e601e074a8a428375cac83f866d37e346da
@mail.webb.ru <78320452+imgod123456@users.noreply.github.com>
2021-10-13 17:41:58 -0700

    Revert "Fix typo in dropdown.tsx"

--- app/src/ui/toolbar/dropdown.tsx
+++ app/src/ui/toolbar/dropdown.tsx
@@ -82,7 +82,7 @@ export interface IToolbarDropdownProps {
   /** The button's style. Defaults to `ToolbarButtonStyle.Standard`. */
   readonly style?: ToolbarButtonStyle
 
-  /** Whether the dropdown will trap focus or not. Defaults to true. */
+  /** Wether the dropdown will trap focus or not. Defaults to true. */
   readonly enableFocusTrap?: boolean
 
   /**
